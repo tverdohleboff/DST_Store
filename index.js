fb1d3b76211c9308823f8dd7b13005b28d851107
Alex <tverdohleboff@gmail.com>
2022-12-14 19:58:52 +0400

    Price Change

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
 
-const a = [
-    {   
-        name: 'Мясо монстра',
-        price: 10,
-        quality: 'good',
-    },
-    {
-        name: 'Трава',
-        price: 5,
-        quality: 'good',
-    },
-    {
-        name: 'Ветка',
-        price: 7,
-        quality: 'good',
-    },
-    {
-        name: 'Камень',
-        price: 4,
-        quality: 'good',
-    }
-];
-const b = {
+const products = {
     'Мясо монстра': {
         name: 'Мясо монстра',
-        price: 10,
+        price: 12,
         quality: 'good',
     },
     'Трава': {
@@ -39,25 +17,16 @@ const b = {
     },
     'Камень': {
         name: 'Камень',
-        price: 4,
+        price: 5,
         quality: 'good',
     },
 };
 
-console.log('a', a);
-console.log('b', b);
+const products_container = document.getElementById('products_container');
+Object.values(products).forEach(element => {
+   
+    let div = document.createElement("div");
+    div.innerHTML=element.name + "/" + element.price + "/" + element.quality;
+    products_container.append(div);
 
-let Stone_Price_Arr = a.find(item => item.name === 'Камень').price;
-console.log('Stone_Price_Arr', Stone_Price_Arr);
-console.log('Stone_Price_Arr2', a[3].price);
-
-let Stone_Price_Obj = b['Камень'].price;
-console.log('Stone_Price_Obj', Stone_Price_Obj);
-console.log('Array From Obj', Object.values(b));
-console.log('Keys Array Obj', Object.keys(b));
-
-let Obj_from_A = {};
-a.forEach(item => {
-    Obj_from_A[item.name] = item;
-})
-console.log('Obj from Arr', Obj_from_A);
+});
