a36c06c212e8debd73ba91d0f05ee1f243745d23
Alex <tverdohleboff@gmail.com>
2022-12-15 19:23:15 +0400

    Background Color при наведении

--- index.js
+++ index.js
@@ -1,84 +1,97 @@
-
 const products = {
     'Мясо монстра': {
         name: 'Мясо монстра',
         price: 12,
         quality: 'good',
+        hint: 'Не похоже на еду',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/0/06/%D0%9C%D1%8F%D1%81%D0%BE_%D0%BC%D0%BE%D0%BD%D1%81%D1%82%D1%80%D0%B0.png/revision/latest?cb=20130621024257&path-prefix=ru',
     },
     'Трава': {
         name: 'Срезанная трава',
         price: 5,
         quality: 'good',
+        hint: 'Срезанная трава, твори-созидай что хочешь!',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/d/df/%D0%A2%D1%80%D0%B0%D0%B2%D0%B0_%D1%80%D0%B0%D1%81%D1%82%D0%B5%D0%BD%D0%B8%D0%B5.png/revision/latest?cb=20220216154527&path-prefix=ru',
     },
     'Ветка': {
         name: 'Ветка',
         price: 6,
         quality: 'good',
+        hint: 'Кучка маленьких веточек. Сгодятся для растопки, между прочим',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/2/2d/%D0%92%D0%B5%D1%82%D0%BA%D0%B8.png/revision/latest?cb=20130517112635&path-prefix=ru',
     },
     'Камень': {
         name: 'Камень',
         price: 3,
         quality: 'good',
+        hint: 'Это камень. Чего тут непонятного?',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/c/c0/%D0%9A%D0%B0%D0%BC%D0%BD%D0%B8.png/revision/latest?cb=20121221191016&path-prefix=ru',
     },
     'Золото': {
         name: 'Золотой самородок',
         price: 60,
         quality: 'good',
+        hint: 'Небольшой кусок золота. Атомный номер 79',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/2/26/%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%BE%D0%B9_%D1%81%D0%B0%D0%BC%D0%BE%D1%80%D0%BE%D0%B4%D0%BE%D0%BA.png/revision/latest?cb=20121221190147&path-prefix=ru',
     },
     'Паутина': {
         name: 'Паутина',
         price: 17,
         quality: 'good',
+        hint: 'Произведено лучшими ткачихами гигантских пауков',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/e/e3/%D0%9F%D0%B0%D1%83%D1%82%D0%B8%D0%BD%D0%B0.png/revision/latest?cb=20130316201238&path-prefix=ru',
     },
     'Камыш': {
         name: 'Срезанный камыш',
         price: 13,
         quality: 'good',
+        hint: 'Срезанный камыш, готовый к обработке',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/3/3b/%D0%A1%D1%80%D0%B5%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%B9_%D0%BA%D0%B0%D0%BC%D1%8B%D1%88.png/revision/latest?cb=20130410011412&path-prefix=ru',
     },
     'Топливо ужаса': {
         name: 'Топливо ужаса',
         price: 33,
         quality: 'good',
+        hint: 'Безумная штука!',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/5/5f/%D0%A2%D0%BE%D0%BF%D0%BB%D0%B8%D0%B2%D0%BE_%D1%83%D0%B6%D0%B0%D1%81%D0%B0.png/revision/latest?cb=20130520103028&path-prefix=ru',
     },
     'Лампочка': {
         name: 'Лампочка',
         price: 5,
         quality: 'good',
+        hint: 'Она медленно угасает',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/1/1e/%D0%9B%D0%B0%D0%BC%D0%BF%D0%BE%D1%87%D0%BA%D0%B0.png/revision/latest?cb=20130522170120&path-prefix=ru',
     },
     'Соты': {
         name: 'Соты',
         price: 18,
         quality: 'good',
+        hint: 'Раньше в них жили пчёлы',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/8/86/%D0%A1%D0%BE%D1%82%D1%8B.png/revision/latest?cb=20130410014543&path-prefix=ru',
     },
     'Кремень': {
         name: 'Кремень',
         price: 3,
         quality: 'good',
+        hint: 'Это очень острый камень',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/b/bd/%D0%9A%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D1%8C.png/revision/latest?cb=20121226110342&path-prefix=ru',
     },
     'Бревно': {
         name: 'Бревно',
         price: 7,
         quality: 'good',
+        hint: 'Оно большое, оно тяжёлое и оно деревянное',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/d/da/%D0%91%D1%80%D0%B5%D0%B2%D0%BD%D0%BE.png/revision/latest?cb=20130410004628&path-prefix=ru',
     },
     'Шестеренка': {
         name: 'Шестеренка',
         price: 23,
         quality: 'good',
+        hint: 'Винтики и шестерёнки',
         image: 'https://static.wikia.nocookie.net/dont-starve/images/0/08/%D0%A8%D0%B5%D1%81%D1%82%D0%B5%D1%80%D1%91%D0%BD%D0%BA%D0%B8.png/revision/latest?cb=20200525082731&path-prefix=ru',
     },
 };
+
 
 const products_container = document.getElementById('productsContainer');
 Object.values(products).forEach(element => {
@@ -98,7 +111,7 @@ Object.values(products).forEach(element => {
 
     const priceDiv = document.createElement("div");
     priceDiv.classList.add('product-price');
-    priceDiv.innerHTML = element.price + ' &#8381;';
+    priceDiv.innerHTML = element.price + ' &#321;';
     productParentDiv.append(priceDiv);
 
     const qualityDiv = document.createElement("div");
@@ -112,5 +125,9 @@ Object.values(products).forEach(element => {
     addToCartButton.setAttribute('type', 'button');
     productParentDiv.append(addToCartButton);
 
+    const hintDiv = document.createElement("div");
+    hintDiv.classList.add('product-hint');
+    hintDiv.innerHTML = element.hint;
+    
     products_container.append(productParentDiv);
 });
